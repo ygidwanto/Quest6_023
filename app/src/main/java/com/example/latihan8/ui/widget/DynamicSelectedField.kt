d5995cd68366a0314f87210835ae1ebf90617316
ygidwanto <yogi.dewanto.ft22@mail.umy.ac.id>
2024-11-22 13:28:31 +0700

    commit 10. membuat dropdown

--- app/src/main/java/com/example/latihan8/ui/widget/DynamicSelectedField.kt
+++ app/src/main/java/com/example/latihan8/ui/widget/DynamicSelectedField.kt
@@ -1,2 +1,18 @@
 package com.example.latihan8.ui.widget
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import java.lang.reflect.Modifier
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DynamicSelectTextField(
+    selectedValue: String,
+    options: List<String>,
+    label: String,
+    onValueChangedEvent: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+
+}
