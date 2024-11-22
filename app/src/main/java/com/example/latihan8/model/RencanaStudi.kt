5ce61c24fd6dd4afd3b35d877e4c52d3ea09e71a
ygidwanto <yogi.dewanto.ft22@mail.umy.ac.id>
2024-11-22 14:00:34 +0700

    commit 18. menambahkan data mahasiswaviewmodel

--- app/src/main/java/com/example/latihan8/model/RencanaStudi.kt
+++ app/src/main/java/com/example/latihan8/model/RencanaStudi.kt
@@ -2,7 +2,7 @@ package com.example.latihan8.model
 
 import com.example.latihan8.data.MataKuliah
 
-class RencanaStudi (
-    val MataKuliah: String = "",
-    val kelas: String = "",
+data class RencanaStudi (
+    val namaMk: String = "",
+    val kelas: String = ""
 )
--- app/src/main/java/com/example/latihan8/ui/view/viewmodel/MahasiswaViewModel.kt
+++ app/src/main/java/com/example/latihan8/ui/view/viewmodel/MahasiswaViewModel.kt
@@ -1,4 +1,27 @@
 package com.example.latihan8.ui.view.viewmodel
 
+import android.R
+import com.example.latihan8.model.RencanaStudi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
 class MahasiswaViewModel {
+    private val _krsState = MutableStateFlow(RencanaStudi())
+    val krsStatusUi: StateFlow<RencanaStudi> = _krsState.asStateFlow()
+
+    fun setMataKuliah(mkPilihan: String){
+        _krsState.update { stateMK -> stateMK.copy(namaMk = mkPilihan) }
+    }
+    fun setKelas(kelasPilihan: String){
+        _krsState.update { stateKelas -> stateKelas.copy(namaMk = kelasPilihan) }
+    }
+    fun saveDataKRS(ls: MutableList<String>){
+        _krsState.update { status -> status.copy(
+            namaMk = ls[0],
+            kelas = ls[1]
+        )
+        }
+    }
 }
