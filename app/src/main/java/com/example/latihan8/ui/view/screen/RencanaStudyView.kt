b5e6fe143d1e4dbb6ba998e4a1c0bff2122e0cfb
ygidwanto <yogi.dewanto.ft22@mail.umy.ac.id>
2024-11-22 12:55:11 +0700

    commit 3: Menambahkan coloum pada box Rencana study

--- app/src/main/java/com/example/latihan8/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/latihan8/ui/view/screen/RencanaStudyView.kt
@@ -88,6 +88,15 @@ fun RencanaStudiView(
                     )
                 )
                 .fillMaxSize(),
-        )
+        ){
+            Column (
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+
+            }
+        }
     }
 }
