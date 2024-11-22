858adead74876ed74349681c8875ae24ea2c2e56
ygidwanto <yogi.dewanto.ft22@mail.umy.ac.id>
2024-11-22 12:53:22 +0700

    commit 3: Menambahkan Box pada Rencana study

--- app/src/main/java/com/example/latihan8/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/latihan8/ui/view/screen/RencanaStudyView.kt
@@ -4,6 +4,7 @@ import android.graphics.Color
 import android.media.Image
 import android.widget.Button
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -11,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -21,6 +23,8 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.latihan8.model.Mahasiswa
 import com.example.latihan8.model.RencanaStudi
 import java.nio.file.WatchEvent
@@ -73,6 +77,17 @@ fun RencanaStudiView(
                 )
             }
         }
-
+        Spacer(modifier = Modifier.padding(top = 16.dp))
+        Box(
+            modifier = Modifier
+                .background(
+                    color = Color.White,
+                    shape = RoundedCornerShape(
+                        topEnd = 15.dp
+                        topStart = 15.dp
+                    )
+                )
+                .fillMaxSize(),
+        )
     }
 }
