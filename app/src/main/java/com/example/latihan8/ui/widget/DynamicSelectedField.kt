2f86ea52c18ab38d7c486be22341c2402ba026ef
ygidwanto <yogi.dewanto.ft22@mail.umy.ac.id>
2024-11-22 13:33:50 +0700

    commit 14. menambahkan warna

--- app/src/main/java/com/example/latihan8/ui/widget/DynamicSelectedField.kt
+++ app/src/main/java/com/example/latihan8/ui/widget/DynamicSelectedField.kt
@@ -4,6 +4,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -37,4 +38,10 @@ fun DynamicSelectTextField(
                     expanded = expanded
                 )
     },
+            colors = OutlinedTextFieldDefaults.colors(),
+            modifier = Modifier
+                .menuAnchor()
+                .fillMaxWidth()
+        )
+
 }
