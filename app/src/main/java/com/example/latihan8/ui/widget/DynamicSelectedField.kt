ae21880279db777038fcd7f7a5bd55ed82a82361
ygidwanto <yogi.dewanto.ft22@mail.umy.ac.id>
2024-11-22 13:39:47 +0700

    commit 16. memperbaiki eroro

--- app/src/main/java/com/example/latihan8/ui/widget/DynamicSelectedField.kt
+++ app/src/main/java/com/example/latihan8/ui/widget/DynamicSelectedField.kt
@@ -1,16 +1,21 @@
 package com.example.latihan8.ui.widget
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import java.lang.reflect.Modifier
-
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -28,7 +33,7 @@ fun DynamicSelectTextField(
         expanded = expanded,
         onExpandedChange = {expanded = !expanded},
         modifier = Modifier
-    ){
+    ) {
         OutlinedTextField(
             readOnly = true,
             value = selectedValue,
@@ -38,12 +43,13 @@ fun DynamicSelectTextField(
                 ExposedDropdownMenuDefaults.TrailingIcon(
                     expanded = expanded
                 )
-    },
+            },
             colors = OutlinedTextFieldDefaults.colors(),
             modifier = Modifier
                 .menuAnchor()
                 .fillMaxWidth()
         )
+
         ExposedDropdownMenu(expanded = expanded,
             onDismissRequest = {expanded = false}) {
             options.forEach { option: String ->
